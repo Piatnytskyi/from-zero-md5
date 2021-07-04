b6111cdbea5317f9bfbed1cd0a7650c7bad512d3
v.piatnytskyi <vitalii.piatnytskyi.pz.2018@lpnu.ua>
2021-07-04 14:37:12 +0300

    Added solution and library project to it.

---
+++ MD5/CustomMD5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MD5
+{
+    public class CustomMD5 : System.Security.Cryptography.MD5
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override byte[] HashFinal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
