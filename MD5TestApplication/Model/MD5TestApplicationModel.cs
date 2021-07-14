8e09d913eb458da6b86f6e2b01c356b6e1992c67
v.piatnytskyi <vitalii.piatnytskyi.pz.2018@lpnu.ua>
2021-07-15 00:46:03 +0300

    Added infrastructure for an WPF MVVM application. Implemented model. Defined view model. Set up view layout and binded commands.

---
+++ MD5TestApplication/Model/MD5TestApplicationModel.cs
@@ -0,0 +1,122 @@
+﻿namespace MD5TestApplication.Model
+{
+    public class MD5TestApplicationModel : AbstractModel
+    {
+        private string _enteredInput;
+
+        public string EnteredInput
+        {
+            get => _enteredInput;
+            set
+            {
+                if (_enteredInput.Equals(value))
+                {
+                    return;
+                }
+                _enteredInput = value;
+                RaisePropertyChanged(nameof(_enteredInput));
+            }
+        }
+
+        private string _filenameInput;
+
+        public string FilenameInput
+        {
+            get => _filenameInput;
+            set
+            {
+                if (_filenameInput.Equals(value))
+                {
+                    return;
+                }
+                _filenameInput = value;
+                RaisePropertyChanged(nameof(_filenameInput));
+            }
+        }
+
+        private string _filenameToCheckInput;
+
+        public string FilenameToCheckInput
+        {
+            get => _filenameToCheckInput;
+            set
+            {
+                if (_filenameToCheckInput.Equals(value))
+                {
+                    return;
+                }
+                _filenameToCheckInput = value;
+                RaisePropertyChanged(nameof(_filenameToCheckInput));
+            }
+        }
+
+        private string _filenameChecksumInput;
+
+        public string FilenameChecksumInput
+        {
+            get => _filenameChecksumInput;
+            set
+            {
+                if (_filenameChecksumInput.Equals(value))
+                {
+                    return;
+                }
+                _filenameChecksumInput = value;
+                RaisePropertyChanged(nameof(_filenameChecksumInput));
+            }
+        }
+
+        private string _output;
+
+        public string Output
+        {
+            get => _output;
+            set
+            {
+                if (_output.Equals(value))
+                {
+                    return;
+                }
+                _output = value;
+                RaisePropertyChanged(nameof(_output));
+            }
+        }
+
+        private int _progressDone;
+
+        public int ProgressDone
+        {
+            get => _progressDone;
+            set
+            {
+                if (_progressDone.Equals(value))
+                {
+                    return;
+                }
+                _progressDone = value;
+                RaisePropertyChanged(nameof(_progressDone));
+            }
+        }
+
+        private int _progressMaximum;
+
+        public int ProgressMaximum
+        {
+            get => _progressMaximum;
+            set
+            {
+                if (_progressMaximum.Equals(value))
+                {
+                    return;
+                }
+                _progressMaximum = value;
+                RaisePropertyChanged(nameof(_progressMaximum));
+            }
+        }
+
+        public bool IsInProggress
+        {
+            get => ProgressDone != ProgressMaximum;
+        }
+    }
+}
---
+++ MD5TestApplication/ViewModel/MD5TestApplicationViewModel.cs
@@ -0,0 +1,93 @@
+﻿using MD5TestApplication.Command;
+using MD5TestApplication.Model;
+
+namespace MD5TestApplication.ViewModel
+{
+    class MD5TestApplicationViewModel
+    {
+        public MD5TestApplicationModel MD5TestApplicationModel { get; set; } = new MD5TestApplicationModel();
+
+        public RelayCommand SaveAsCommand { get; set; }
+
+        public RelayCommand HashCommand { get; set; }
+
+        public RelayCommand ChooseFileToHashCommand { get; set; }
+        public RelayCommand HashFileCommand { get; set; }
+
+        public RelayCommand ChooseFileAndChecksumCommand { get; set; }
+        public RelayCommand CompareFileHashAndChecksumCommand { get; set; }
+
+        public MD5TestApplicationViewModel()
+        {
+            SaveAsCommand = new RelayCommand(o => SaveAs(), c => CanSaveAs());
+
+            HashCommand = new RelayCommand(o => Hash(), c => CanHash());
+
+            ChooseFileToHashCommand = new RelayCommand(o => ChooseFileToHash(), c => CanChooseFileToHash());
+            HashFileCommand = new RelayCommand(o => HashFile(), c => CanHashFile());
+
+            ChooseFileAndChecksumCommand = new RelayCommand(o => ChooseFileAndChecksum(), c => CanChooseFileAndChecksum());
+            CompareFileHashAndChecksumCommand = new RelayCommand(o => CompareFileHashAndChecksum(), c => CanCompareFileHashAndChecksum());
+        }
+
+        private void SaveAs()
+        {
+
+        }
+
+        private bool CanSaveAs()
+        {
+            return !string.IsNullOrEmpty(MD5TestApplicationModel.Output);
+        }
+
+        private void Hash()
+        {
+            
+        }
+
+        private bool CanHash()
+        {
+            return false;
+        }
+
+        private void ChooseFileToHash()
+        {
+
+        }
+
+        private bool CanChooseFileToHash()
+        {
+            return false;
+        }
+
+        private void HashFile()
+        {
+
+        }
+
+        private bool CanHashFile()
+        {
+            return false;
+        }
+
+        private void ChooseFileAndChecksum()
+        {
+            
+        }
+
+        private bool CanChooseFileAndChecksum()
+        {
+            return false;
+        }
+
+        private void CompareFileHashAndChecksum()
+        {
+
+        }
+
+        private bool CanCompareFileHashAndChecksum()
+        {
+            return false;
+        }
+    }
+}
